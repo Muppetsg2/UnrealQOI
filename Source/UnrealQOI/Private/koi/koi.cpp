f0abeeeff13da247631f78f5fdaf2d31511e8b72
Muppetsg2 <54802675+Muppetsg2@users.noreply.github.com>
2025-11-20 20:21:45 +0100

    koi lib update
    - koi_image.h v1.01
    - koi_image_write.h v1.01

--- Source/UnrealQOI/Private/koi/koi.cpp
+++ Source/UnrealQOI/Private/koi/koi.cpp
@@ -4,9 +4,10 @@
 #define KOI_IMAGE_IMPLEMENTATION
 #define KOI_ONLY_QOI
 #define KOI_NO_STDIO
+#define KOI_NO_LINEAR
 #include "koi_image.h"
 
 #define KOI_IMAGE_WRITE_IMPLEMENTATION
-#define KOIW_ONLY_QOI
+#define KOI_WRITE_ONLY_QOI
 #define KOI_WRITE_NO_STDIO
 #include "koi_image_write.h"
